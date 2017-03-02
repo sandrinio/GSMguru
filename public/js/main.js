15158bc726eac4bc9ae367da6d64784c09aebced
sandrinio <sandro.suladze@gmail.com>
2017-03-02 15:03:18 +0400

    working on landing page v2

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
 
-  setTimeout(function() {
-    $("#loadLater").show()
-  }, 1500);
-
-  setTimeout(function () {
-    $('#showLater2').show()
-  }, 2000);
+  // setTimeout(function() {
+  //   $("#loadLater").show()
+  // }, 1500);
+  //
+  // setTimeout(function () {
+  //   $('#showLater2').show()
+  // }, 2000);
 
 
 });
